12d26ed223219f1af81c81c604afa56a5c6bfbe7
Burak Akdogan <zeliha.bekcan@gmail.com>
2022-09-26 19:30:17 +0300

    26.7

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,9 +5,9 @@
         <div class="flex-auto" style="color: white;">We Care About Humanity!</div>
         
       <div>
-        <a href="https://www.facebook.com/WHO/"><i class="fab fa-facebook fa-xl pr-2"></i></a>
-        <a href="https://twitter.com/WHO"><i class="fab fa-twitter fa-xl pr-2"></i></a>
-        <a href="https://covid19responsefund.org/en/"><i class="fas fa-donate fa-xl pr-2"></i></a>
+        <a href="https://www.facebook.com/WHO/"><i class="fab fa-facebook fa-xl pr-3"></i></a>
+        <a href="https://twitter.com/WHO"><i class="fab fa-twitter fa-xl pr-3"></i></a>
+        <a href="https://covid19responsefund.org/en/"><i class="fas fa-donate fa-xl pr-3"></i></a>
       </div>
         <a v-for="(link, i) in links" :key="i" :href="link" class="text-black" target="_blank">
           <Icon :name="i" class="ml-4" />
