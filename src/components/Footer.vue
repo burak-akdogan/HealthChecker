4cc6f6d20a60bca4de456f4adccc5335bdd3e1fe
Burak Akdogan <zeliha.bekcan@gmail.com>
2022-09-26 15:47:36 +0300

    26.5

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,19 @@
     <div class="container-lg p-responsive d-flex">
       <div class="flex-auto" style="color: white;">HealthChecker © 2022</div >
         <div class="flex-auto" style="color: white;">We Care About Humanity!</div>
+        
       <div>
+        <a href="https://www.facebook.com/WHO/"><i class="fab fa-facebook fa-xl pr-2"></i></a>
+        <a href="https://twitter.com/WHO"><i class="fab fa-twitter fa-xl pr-2"></i></a>
+        <a href="https://covid19responsefund.org/en/"><i class="fas fa-donate fa-xl pr-2"></i></a>
+      </div>
         <a v-for="(link, i) in links" :key="i" :href="link" class="text-black" target="_blank">
           <Icon :name="i" class="ml-4" />
+          
         </a>
       </div>
     </div>
-  </div>
+  
 </template>
 
 <script>
@@ -19,13 +25,7 @@ export default {
   data() {
     return {
       pkg,
-      links: {
-        instagram: 'https://www.instagram.com/healthcheck__',
-        facebook: 'https://www.facebook.com/',
-        amazon: 'https://www.amazon.com/'
-    
-        
-      }
+      
     };
   }
 };
