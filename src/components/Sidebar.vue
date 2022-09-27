09ea24fdd751d301388005f48809dee632bb8523
Burak Akdogan <zeliha.bekcan@gmail.com>
2022-09-27 22:06:09 +0300

    newlogo

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="sidebar" class="position-fixed py-lg-4">
     <router-link :to="`/`" class="hide-sm hide-md hide-lg" >
-      <img  src="~/@/assets/onlylogo.png" style="max-width: 220px;" class="mb-2 anim-pulse" />
+      <img  src="~/@/assets/onlylogo2.png" style="max-width: 220px;" class="mb-2 anim-pulse" />
     </router-link>
     <ul class="list-style-none border-top border-lg-top-0 text-center text-lg-left">
       <li class="col-3 col-lg-12">
