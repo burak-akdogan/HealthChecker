aab705aa648d43f8f63e8056271165434a48e577
Burak Akdogan <zeliha.bekcan@gmail.com>
2022-10-08 00:18:26 +0300

    10/8

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,10 @@
     <!-- <div class="hero py-6" > -->
       <div class="hero py-2" >
       <div class="container-lg p-responsive pt-10">
-        <div class="col-md-5">
+        <div class="col-md-4">
           <div></div>
         
-          <img class="eyebrow-mb-2 " src="~/@/assets/logov10.png"><div></div>
+          <img class="eyebrow-mb-1" src="~/@/assets/logov10.png"><div></div>
           <h2 class="eyebrow-mb-3" style="color:#11639d">Protect Your Employees And Customers.</h2>
           <br>
           <h1 class="mb-4" style="color:#303247" v-text="'Welcome'"/> 
